0e2974e6518fb1bc916e3aa668f930d559b6f1f5
Gabe <jflores353@gmail.com>
2019-03-15 14:51:53 -0500

    created Reducers folder and index.js inside of it

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import state from "./state";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const {
+dateTime,
+newComments,
+newTasks,
+newOrders,
+tickets,
+orders,
+taskItem,
+tasks,
+messages
+} = state;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    
+    <App 
+      taskItem={taskItem}
+      dateTime={dateTime}
+      newComments={newComments}
+      newTasks={newTasks}
+      newOrders={newOrders}
+      tickets={tickets}
+      orders={orders} 
+      messages={messages}
+      tasks={tasks}
+    />
+  ,
+  document.getElementById("root")
+);
